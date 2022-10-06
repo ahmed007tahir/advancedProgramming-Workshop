1fb8122021c7be6a84a078f0b07f327e4a660b59
ahmed007tahir <ahmed007tahir@gmail.com>
2022-10-06 17:53:03 +0100

    Task 2

--- Task 2/main.cpp
+++ Task 2/main.cpp
@@ -1,30 +1,60 @@
 #include <iostream>
 using namespace std;
 
-string cypher(string text, int function, int key)
+void cypher(string text, int flag, int key)
 {
-    if (function == 0) // encrypt
+
+    // this value will be added to the ascii value of the char
+    int shift = 0;
+    int i = 0;
+    int newposition = 0;
+    bool capital = false;
+
+    if (flag == 0) // encrypt
     {
-        for(char& c : text) {
-            if (c < 123 && c > 96){
-                int newValue = int(c) + key;
-                cout << (char)newValue;
-            }
-            else
-            {
-                cout << c;
-            }
+        shift += key;
+    }
+    else if (flag == 1) // decrypt
+    {
+        shift -= key;
+    }
+
+    for(char& c : text) // iterate through string
+    {
+        if (c > 64 && c < 91)  // if ascci value belongs to capital alphabets
+        {
+            i = int(c) - 64;
+            capital = true;
+        }
+
+        else if (c > 96 && c < 123) // if ascci value belongs to lower case alphabets
+        {
+            i = int(c) - 96;
+            capital = false;
+        }
+        newposition = i + shift;
+        if (newposition > 26)
+        {
+            newposition %= 26;
+        }
+        else if (newposition < 1)
+        {
+            newposition %= 26;
+            newposition += 26;
+        }
+        if (capital)
+        {
+            cout << char(newposition + 64);
+        }
+        else
+        {
+            cout << char(newposition + 96);
         }
     }
-//    else if (function == 1) // decrypt
-//    {
-//
-//    }
-    return text;
 }
 
 
 int main() {
-    cypher("Hello World!",0,3);
+    cypher("Abc",1,27);
     return 0;
 }
