9a3e2b2ffef1095a91d0d0ea13d1369dfb87bc85
ahmed007tahir <ahmed007tahir@gmail.com>
2022-10-10 14:26:29 +0100

    Week 2

---
+++ WordFrequencyPair.h
@@ -0,0 +1,23 @@
+//
+// Created by ahmed on 10/10/2022.
+//
+
+#ifndef WORKSHOP_2_WORDFREQUENCYPAIR_H
+#define WORKSHOP_2_WORDFREQUENCYPAIR_H
+
+
+class WordFrequencyPair {
+private:
+
+public:
+    static std::string TextReader(const std::string& fileName);
+    static int NumberOfSentences(const std::string& text);
+    static std::vector<WordFrequencyPair> ListFrequencyPairs(const std::string& text);
+
+    std::string word;
+    int frequency;
+
+};
+
+
+#endif //WORKSHOP_2_WORDFREQUENCYPAIR_H
---
+++ cmake-build-debug/WordFrequencyPair.cpp
@@ -0,0 +1,68 @@
+//
+// Created by ahmed on 10/10/2022.
+//
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "../WordFrequencyPair.h"
+
+std::string WordFrequencyPair::TextReader(const std::string& fileName) {
+
+    std::string line;
+    std::string text;
+
+    std::ifstream myFile (fileName);
+
+    if (myFile.is_open()) {
+        while (getline(myFile, line)) {
+            text += line;
+            text += "\n";
+            }
+        myFile.close();
+    }
+    else std::cout << "Unable to open file";
+
+    std::cout << text << std::endl;
+
+    return text;
+}
+
+
+int WordFrequencyPair::NumberOfSentences(const std::string& text) {
+
+    int countSentences = 0;
+
+    // count number of sentences
+    for(char c : text) // iterate through string
+    {
+        // looks for punctuation marking end of sentence
+        if (c == '.' || c == '!' || c == '?')
+        {
+            countSentences++;
+        }
+    }
+    return countSentences;
+}
+
+std::vector<WordFrequencyPair> WordFrequencyPair::ListFrequencyPairs(const std::string& text){
+
+    int startIndexPointer = 0;
+    int endIndexPointer = 0;
+    int n = 0;
+
+    std::vector<WordFrequencyPair> hello;
+
+    for(int i = 0; i < text.length(); i++){
+        char x = text[i];
+        if (x == ' ' || x == ',' || x ==  '\n' || x ==  '!' || x ==  '.' || x ==  ':' || x ==  ';' || x ==  '?') {
+            endIndexPointer = i;
+            n = endIndexPointer - startIndexPointer;
+            std::cout << text.substr(startIndexPointer, n) << std::endl;
+            startIndexPointer = endIndexPointer + 1;
+        }
+
+        // now i have the beginning and end index of the first word that needs to be added to the vector.
+    }
+    return hello;
+}
