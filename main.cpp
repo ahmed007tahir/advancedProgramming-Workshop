8f4de74c230819b0060959b6efce408971bc39c0
ahmed007tahir <ahmed007tahir@gmail.com>
2022-10-10 14:26:29 +0100

    Week 2

--- main.cpp
+++ main.cpp
@@ -1,48 +1,14 @@
 #include <iostream>
-#include <fstream>
 #include <string>
-using namespace std;
+#include <vector>
 
-
-void textReader(string fileName){
-    string line;
-    int countSentences = 0;
-    int countWords = 0;
-    ifstream myfile (fileName);
-
-    if (myfile.is_open())
-    {
-        while ( getline (myfile,line) )
-        {
-            cout << line << '\n';
-
-            // count number of sentences
-            for(char& c : line) // iterate through string
-            {
-                if (c == '.' || c == '!' || c == '?'){
-                    countSentences++;
-                }
-                if (c == ' ' || c == ',')
-                {
-                    countWords ++;
-
-                }
-            }
-        }
-        countWords += countSentences;
-
-        cout << "\nThere are " << countSentences << " sentences" << endl;
-        cout << "\nThere are " << countWords << " words" << endl;
-
-        myfile.close();
-    }
-    else cout << "Unable to open file";
-}
-
+#include "WordFrequencyPair.h"
 
 int main () {
 
-    textReader(R"(C:\Users\ahmed\Desktop\OneDrive - University of Lincoln\Year 2\Advanced Programming\Week 2\Workshop 2\Text\input.txt)");
+    std::string text = WordFrequencyPair::TextReader(R"(C:\Users\ahmed\Desktop\OneDrive - University of Lincoln\Year 2\Advanced Programming\Week 2\Workshop 2\Text\input.txt)");
+    int sentences = WordFrequencyPair::NumberOfSentences(text);
+    std::cout << "Number of Sentences: " << sentences << std::endl;
 
     return 0;
 }
